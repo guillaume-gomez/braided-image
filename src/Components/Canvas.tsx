3ebbf335afd3123d4ec60f67b3c532ccd95b0b92
Guillaume Gomez <guillaume.gomez846@gmail.com>
2022-08-17 22:24:08 +0200

    :construction: first try render images

--- src/Components/Canvas.tsx
+++ src/Components/Canvas.tsx
@@ -30,28 +30,43 @@ function Canvas({ image1, image2 }: CanvasInterface) {
 
   function draw(context: CanvasRenderingContext2D, widthCanvas: number, heightCanvas: number) {
     context.clearRect(0,0, width, height);
-    console.log(image1.width)
-    console.log(image1.height)
+    //console.log(image1.width)
+    //console.log(image1.height)
     context.drawImage(image1,0,0, image1.width, image1.height, 0, 0, widthCanvas, heightCanvas);
-    drawBraid(context, image2);
+    let x = 0;
+    while(x < widthCanvas) {
+      drawBraid(context, image2, x, heightCanvas);
+      x += 80;
+
+    }
+    //drawBraidPart(context, image2, 50,60, 80,  60);
   }
 
-  function drawBraid(context: CanvasRenderingContext2D, image: HTMLImageElement) {
-    const braidSize = Math.floor(Math.random() * 40);
+  function drawBraid(context: CanvasRenderingContext2D, image: HTMLImageElement, x: number, height: number) {
+    const braidSize = Math.floor(Math.random() * 100);
     let y = 0;
-    let x = 200;
+    const heightCross = 50;
     while(y < height) {
-      drawBraidPart(context, image2, x, y, 40, 50);
-      y += 50;
+      drawBraidPart(context, image2, x, y, 40, heightCross);
+      y += heightCross + 20;
     }
   }
 
+
   function drawBraidPart(context: CanvasRenderingContext2D, image: HTMLImageElement, x: number, y: number, widthBraid: number, heightBraid: number) {
     context.drawImage(image, x, y, widthBraid, heightBraid, x, y, widthBraid, heightBraid);
+    context.beginPath();
+    context.moveTo(x,y);
+    context.lineTo(x, y + heightBraid);
+    context.stroke();
+    context.moveTo(x + widthBraid,y);
+    context.lineTo(x + widthBraid, y + heightBraid);
+    context.stroke();
+    //context.strokeRect(x-1, y, widthBraid, heightBraid-4);
   }
 
   return (
-    <canvas ref={canvasRef} width={width} height={height} style={{border: "2px solid teal"}}/>
+    <canvas ref={canvasRef} width={width} height={height} style={{border: "5px solid teal"}}/>
   );
 }
 
