1b4aeb4c09b7ee0dabe386d40274bfda69444211
Guillaume Gomez <guillaume.gomez846@gmail.com>
2022-11-09 00:22:04 +0100

    add card as layout for the result

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,8 @@ import './App.css';
 import InputImage from "./Components/InputImage";
 import Form from "./Components/Form";
 import Canvas from "./Components/Canvas";
-
+import Drawer from "./Components/Drawer";
+import Footer from "./Components/Footer";
 
 function App() {
   const [image1, setImage1] = useState<HTMLImageElement|null>(null);
@@ -17,13 +18,22 @@ function App() {
   }
 
   return (
-    <div className="">
-    <Form onSubmit={onSubmit} />
-    {
-      image1 && image2 ? 
-        <Canvas image1={image1} image2={image2} padding={padding} /> :
-        <progress className="progress progress-accent w-56"></progress>
-    }
+    <div className="flex flex-col h-screen justify-between">
+      <Drawer/>
+      <div className="mb-auto flex sm:flex-row flex-col items-center justify-between p-2 gap-3">
+        <Form onSubmit={onSubmit} />
+        <div className="card bg-base-200 shadow-xl h-full w-full">
+          <div className="card-body items-center text-center">
+            <h2 className="card-title">Result</h2>
+            {
+              image1 && image2 ?
+              <Canvas image1={image1} image2={image2} padding={padding} /> :
+              <progress className="progress progress-accent w-56"></progress>
+            }
+          </div>
+        </div>
+      </div>
+      <Footer/>
     </div>
   );
 }
